f59382e4a39e6ce16cf5b9ee307a1beac64e6ad2
toastycoder0 <re-jos2002@proton.me>
2025-10-23 11:41:08 -0600

    fix: corrected caso_2 update

--- t2/caso_2/CalificacionTemaFdp.java
+++ t2/caso_2/CalificacionTemaFdp.java
@@ -2,26 +2,31 @@
 // Mostrar la calificación de un tema de la materia de Fundamentos de programación
 public class CalificacionTemaFdp {
   public static void main(String[] args) {
-    // Definimos las variables de entrada
+    // Definimos la variable de entrada
+    int calificaciones;
+
+    // Definimos las variables de salida
     int actualizacion;
     int cuestionario;
     int exposicion;
     int apuntes;
-
-    // Definimos la variable de salida
     int calificacion;
 
-    // Asignamos valores iniciales a las variables de entrada
-    actualizacion = 15;
-    cuestionario = 20;
-    exposicion = 15;
-    apuntes = 10;
+    // Asignamos valores iniciales a la variable de entrada
+    calificaciones = 35302015;
 
     // Inicializamos la variable de salida
+    actualizacion = 0;
+    cuestionario = 0;
+    exposicion = 0;
+    apuntes = 0;
     calificacion = 0;
 
-    // Asignamos valor a la variable de salida
-    // Calculamos la calificación sumando las variables de entrada
+    // Asignamos valor a las variables de salida
+    actualizacion = calificaciones / 1000000;
+    cuestionario = (calificaciones / 10000) % 100;
+    exposicion = (calificaciones / 100) % 100;
+    apuntes = calificaciones % 100;
     calificacion = actualizacion + cuestionario + exposicion + apuntes;
 
     // Mostramos la variable de salida "calificacion"

38431ec28b159d2862034229ca753c521ead9b03
toastycoder0 <re-jos2002@proton.me>
2025-10-23 09:10:57 -0600

    feat: updated case study

--- t2/caso_2/CalificacionTemaFdp.java
+++ t2/caso_2/CalificacionTemaFdp.java
@@ -2,30 +2,33 @@
 // Mostrar la calificación de un tema de la materia de Fundamentos de programación
 public class CalificacionTemaFdp {
   public static void main(String[] args) {
-    // Definimos las variables de entrada "cuestionario" y "actualizacion"
-    int cuestionario;
+    // Definimos las variables de entrada
     int actualizacion;
+    int cuestionario;
+    int exposicion;
+    int apuntes;
 
-    // Definimos la variable de salida "calificacion"
+    // Definimos la variable de salida
     int calificacion;
 
-    // Asignamos valores iniciales a las variables de entrada "cuestionario" y
-    // "actualizacion"
-    cuestionario = 50;
-    actualizacion = 50;
+    // Asignamos valores iniciales a las variables de entrada
+    actualizacion = 15;
+    cuestionario = 20;
+    exposicion = 15;
+    apuntes = 10;
 
-    // Inicializamos la variable de salida "porcentaje"
+    // Inicializamos la variable de salida
     calificacion = 0;
 
-    // Asignamos valor a la variable de salida "calificacion"
-    // Calculamos la calificación sumando las variables de entrada "cuestionario" y
-    // "actualizacion"
-    calificacion = cuestionario + actualizacion;
+    // Asignamos valor a la variable de salida
+    // Calculamos la calificación sumando las variables de entrada
+    calificacion = actualizacion + cuestionario + exposicion + apuntes;
 
     // Mostramos la variable de salida "calificacion"
-    System.out.println(
-        "La calificación del tema de Fundamentos de Programación es de " + calificacion
-
-    );
+    System.out.println(actualizacion + " actualización");
+    System.out.println(cuestionario + " cuestionario");
+    System.out.println(exposicion + " exposición");
+    System.out.println(apuntes + " apuntes");
+    System.out.println("Calificación " + calificacion);
   }
 }
